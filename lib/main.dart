7ef22e981fd1592acedbb2b6a79d6c8f17e36c11
Denis Bystruev <github@learnSwift.ru>
2020-01-05 22:45:33 +0300

    Finished the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,80 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
+        backgroundColor: Colors.red,
         body: DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int _leftDiceNumber;
+  int _rightDiceNumber;
+  Random _random = Random();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  child: Image.asset('images/dice$_leftDiceNumber.png'),
+                  onPressed: _rollDice,
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  child: Image.asset('images/dice$_rightDiceNumber.png'),
+                  onPressed: _rollDice,
+                ),
+              ),
+            ],
+          ),
+          flex: 3,
+        ),
+        Expanded(
+          child: Text(
+            '${_leftDiceNumber + _rightDiceNumber}',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 75,
+            ),
+          ),
+          flex: 2,
+        ),
+      ],
+      mainAxisAlignment: MainAxisAlignment.center,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _rollDice();
+  }
+
+  void _rollDice() {
+    setState(() {
+      _leftDiceNumber = _random.nextInt(6) + 1;
+      _rightDiceNumber = _random.nextInt(6) + 1;
+    });
   }
 }
